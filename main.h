ee43bdc3d2a13e6cc1dbfbdf2661c0ff319fd1c1
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-06-16 05:35:36 +0100

    Update main.h

--- main.h
+++ main.h
@@ -27,15 +27,15 @@ char *name;
 int hist;
 
 /**
- *struct list_s - A new struct type defining a linked list.
- *@dir: A directory path.
- *@next: A pointer to another struct list_s.
+*struct list_s - A new struct type defining a linked list.
+*@dir: A directory path.
+*@next: A pointer to another struct list_s.
  
- **/
- * struct list_s - A new struct type defining a linked list.
- * @dir: A directory path.
- * @next: A pointer to another struct list_s.
- */
+**/
+* struct list_s - A new struct type defining a linked list.
+* @dir: A directory path.
+* @next: A pointer to another struct list_s.
+*/
 	 
 typedef struct list_s
 {
@@ -44,15 +44,15 @@ typedef struct list_s
 } list_t;
 
 /**
- *struct builtin_s - A new struct type defining builtin commands.
- *@name: The name of the builtin command.
- *@f: A function pointer to the builtin command's function.
+*struct builtin_s - A new struct type defining builtin commands.
+*@name: The name of the builtin command.
+*@f: A function pointer to the builtin command's function.
  
- **/
- * struct builtin_s - A new struct type defining builtin commands.
- * @name: The name of the builtin command.
- * @f: A function pointer to the builtin command's function.
- */
+/**
+* struct builtin_s - A new struct type defining builtin commands.
+* @name: The name of the builtin command.
+* @f: A function pointer to the builtin command's function.
+*/
 
 typedef struct builtin_s
 {
@@ -61,17 +61,17 @@ typedef struct builtin_s
 } builtin_t;
 
 /**
- *struct alias_s - A new struct defining aliases.
- *@name: The name of the alias.
- *@value: The value of the alias.
- *@next: A pointer to another struct alias_s.
+*struct alias_s - A new struct defining aliases.
+*@name: The name of the alias.
+*@value: The value of the alias.
+*@next: A pointer to another struct alias_s.
  
- **/
- * struct alias_s - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct alias_s.
- */
+/**
+* struct alias_s - A new struct defining aliases.
+* @name: The name of the alias.
+* @value: The value of the alias.
+* @next: A pointer to another struct alias_s.
+*/
 
 typedef struct alias_s
 {
