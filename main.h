1b7222ab77fb725cede537549dcc3fe9c97767e2
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-06-17 12:11:56 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,9 +11,11 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <stdbool.h>
+
 /* environment variables */
 extern char **environ;
 extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
 /* handle built ins */
 int checker(char **cmd, char *buf);
 void prompt_user(void);
@@ -45,7 +48,6 @@ char *test_path(char **path, char *command);
 char *append_path(char *path, char *command);
 int handle_builtin(char **command, char *line);
 void exit_cmd(char **command, char *line);
-
 void print_env(void);
 
 /* string handlers */
@@ -54,7 +56,6 @@ int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
 char *_strdup(char *s);
 char *_strchr(char *s, char c);
-
 void execution(char *cp, char **cmd);
 char *find_path(void);
 
@@ -66,15 +67,13 @@ struct builtin
 	char *env;
 	char *exit;
 } builtin;
-
-struct info
+struct  info
 {
 	int final_exit;
 	int ln_count;
 } info;
-struct flags
-
-struct flags
+struct  flags
+struct  flags
 
 {
 	bool interactive;
