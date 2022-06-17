4dccc0becbe7ab840a9f92e062e669737dce745a
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-06-17 12:08:53 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,11 +10,9 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <stdbool.h>
-
 /* environment variables */
 extern char **environ;
 extern __sighandler_t signal(int __sig, __sighandler_t __handler);
-
 /* handle built ins */
 int checker(char **cmd, char *buf);
 void prompt_user(void);
